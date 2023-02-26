efc09329b380633461494b079669872ff4eaf4ce
Vrushali Sirsath <95585241+Vrushalisirsat@users.noreply.github.com>
2023-02-26 20:17:03 +0530

    Update recursion7.java

--- Recursion/recursion7.java
+++ Recursion/recursion7.java
@@ -1,3 +1,5 @@
+// Problems based on Recursion - 4 :Assignment Questions
+
 // Q1 - Given a number n. Print if it is an armstrong number or not.
 // An armstrong number is a number if the sum of every digit in that number raised to the power of total digits in that number is equal to the number.
 // Example : 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 hence 153 is an armstrong number.
